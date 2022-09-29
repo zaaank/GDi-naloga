07d0ec011e9791076ca80c199ce2e46c0018da84
z.smolic <zan.smolic@ineor.si>
2022-09-29 09:52:14 +0200

    file download and delete works

--- DownloadZIP/Program.cs
+++ DownloadZIP/Program.cs
@@ -1,15 +1,13 @@
 ﻿using System.IO.Compression;
 using System.Net;
 
+string zipFilePath = "FILE1.zip";
 WebClient webClient = new WebClient();
-webClient.Headers.Add("Accept: text/html, application/xhtml+xml, /");
-webClient.Headers.Add("User-Agent: Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)");
-webClient.DownloadFile(new Uri("http://datoteke.durs.gov.si/DURS_zavezanci_PO.zip"), "test250.zip");
-string zipFilePath = "test250.zip";
 try
 {
-    ZipFile.ExtractToDirectory(zipFilePath, Environment.CurrentDirectory);
-
+    webClient.DownloadFile(new Uri("http://datoteke.durs.gov.si/DURS_zavezanci_PO.zip"), zipFilePath);
+    ZipFile.ExtractToDirectory(zipFilePath, @"../../../zip");
+    File.Delete("./" + zipFilePath);
 }
 catch
 {
